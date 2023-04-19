47b36ac1b206cd086fa21e9ee9ff523cb7d4e6a6
Gerry Wu <ycwu@crunchbase.com>
2023-04-19 15:57:40 -0500

    install mapbox-gl

--- client/src/index.tsx
+++ client/src/index.tsx
@@ -3,6 +3,10 @@ import ReactDOM from "react-dom";
 import { App } from "./App";
 import "./index.css";
 import * as serviceWorker from "./serviceWorker";
+import mapboxgl from "mapbox-gl";
+// eslint-disable-next-line @typescript-eslint/ban-ts-comment
+// @ts-ignore
+mapboxgl.workerClass = require("worker-loader!mapbox-gl/dist/mapbox-gl-csp-worker").default; // eslint-disable-line @typescript-eslint/no-var-requires
 
 ReactDOM.render(<App />, document.getElementById("root"));
 
